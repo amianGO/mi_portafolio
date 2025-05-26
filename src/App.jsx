f990f0efa2b69b9e58018019826f7ff494b29357
@amianGO <damiangaviria8@gmail.com>
2025-05-25 22:59:49 -0500

    Arreglando Errores de despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 /* Paso 4 Inicio*/
-import { div } from 'framer-motion/client'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Hero from './pages/Hero'
 import ContactForm from './components/contactForm'
 
 function App(){
   return (
-    <Routes>
-      <Route path='/' element={<Hero />} />
-      <Route path='/contacto' element={<ContactForm />} />
-      <Route path='*' element={<div className='text-red-500'>404</div>} />
-    </Routes>
+    
+      <Routes>
+        <Route path='/' element={<Hero />} />
+        <Route path='/contacto' element={<ContactForm />} />
+        <Route path='*' element={<div className='text-red-500'>404</div>} />
+      </Routes>
+    
   )
 }
 
