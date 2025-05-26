78082f005f44c0f858a3500cedd2f717c4b29098
@amianGO <damiangaviria8@gmail.com>
2025-05-25 23:40:20 -0500

    Mejoras

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   base:'/mi_portafolio/',
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html',
+          dest: '',
+          rename: '404.html'
+        }
+      ]
+    })
+  ],
 })
